ba73336bb55c3c5fc39d1909ced9ab8bd29f88af
meysiolio <saturninsky@gmail.com>
2021-09-06 22:56:12 +0430

    Create runner-up-score.py

---
+++ runner-up-score.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    L = list(set(arr))
+    
+    maximum = max(L)
+    max_index = [i for i,j in enumerate(L) if j == maximum]
+    
+    for k in max_index:
+        del L[k]
+
+    print(max(L))
